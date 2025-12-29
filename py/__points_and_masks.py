3D & rotation ImageDrawing update + minor fixes

py/__points_and_masks.py
@@ -39,7 +39,7 @@ def radians_to_vectors(radians):
 def vectors_to_radians(vectors):
     return np.arctan2(vectors[:,1], vectors[:,0])
 
-def bounding_box(points):
+def bounding_box(points): # n-dimensional
     return np.min(points, axis=0), np.max(points, axis=0)
 
 def bounding_box_corners(points):
@@ -48,6 +48,22 @@ def bounding_box_corners(points):
     bl = asd[::2]
     tr = asd[::-2]
     return tl, tr, br, bl
+
+def bounding_corners(points): # n-dimensional
+    # iterate every dimensional corner
+    # 2D -> 4
+    # 3D -> 8
+    ndim = points.shape[1]
+    tl, br = bounding_box(points)
+    mask = np.zeros(ndim, dtype=np.bool_)
+    while 1:
+        corner = tl.copy()
+        corner[mask] = br[mask]
+        yield corner
+        if mask.all(): break
+        j = np.argmin(mask)
+        mask[j] = True
+        mask[:j] = False
 
 def point_normals(points):
     diff = np.diff(points, prepend=points[-1:], axis=0)
@@ -320,8 +336,6 @@ def hausdorff_distance5(points1, points2): # NOT QUITE AS FAST AS 4, but indiffe
     dist2 = distance(*line2)
     if dist1>dist2: return dist1, line1
     return dist2, line2
-
-
     
 def divide_to_sectors(points): # n-dimensional
     # yield batches of points based on what dimensional corner direction is the closest
@@ -475,7 +489,7 @@ def image_shape(resolution, max_x, max_y, *args):
     elif width<height: width += 1
     return width, height
 
-def pixelize_points(points, topleft, bottomright, resolution):
+def pixelize_points(points, topleft, bottomright, resolution): # n-dimensional
     pixels = points-topleft
     scale = (bottomright-topleft).max()
     if scale!=0: pixels /= scale
@@ -489,7 +503,7 @@ def pixelize_distances(values, topleft, bottomright, resolution):
     pixels *= resolution-1
     return pixels.astype(np.int32)
 
-def pointify_pixels(pixels, topleft, bottomright, resolution):
+def pointify_pixels(pixels, topleft, bottomright, resolution): # n-dimensional
     points = pixels.astype(np.float64)
     points -= .5
     points /= resolution-1
@@ -508,8 +522,8 @@ def circle_mask(r, inner=0):
     elif inner<0: mask *= dist_from_center>r+inner
     return mask
 
-def line_mask(start, end): # mask_line
-    offset = np.subtract(end, start).astype(np.float64)
+def line_mask(start, end): # 3d points are fine -> only uses x and y
+    offset = np.subtract(end, start).astype(np.float64)[:2]
     dist = np.linalg.norm(offset)
     if dist<1: return
     _from = (0 if offset[0]>0 else -offset[0],0 if offset[1]>0 else -offset[1])

py/_gui_matplotlib.py
@@ -5,7 +5,9 @@ from matplotlib.figure import Figure
 import matplotlib
 matplotlib.use("TkAgg")
 
-class Viewport(ViewportTemplate):
+from __system import function_timer
+
+class Viewport():
     figure = None
     subplot = None
     canvas = None
@@ -28,3 +30,51 @@ class Viewport(ViewportTemplate):
         self.subplot.imshow(image, extent=(tl[0], br[0], tl[1], br[1]))
         self.subplot.set_title(title)
         self.canvas.draw()
+
+    def get_widget(self):
+        return self.canvas.get_tk_widget()
+
+
+##class Viewport3D(Viewport):
+##    camera_pos = None
+##    
+##    camera_yaw = 0 # yaw
+##    z_rotation = 0 # tilt
+##    x_rotation = 0 # pitch
+##
+##    points = None
+##    normals = None
+##    
+##    def __init__(self, *args, **kwargs):
+##        super().__init__(*args, **kwargs)
+##        self.camera_pos = np.zeros(3)
+##        self.objects = []
+##    
+##    def start_plotting(self):
+##        self.subplot.clear()
+##        
+##    def plot_points(self, points, size=None, color=None):
+##        self.subplot.scatter(points[:,0], points[:,1], alpha=1-points[:,2])
+##
+##    def finish_plotting(self, title=None):
+##        self.subplot.set_title(title)
+##        self.canvas.draw()
+##
+##    def test(self, event):
+##        self.start_plotting()
+##
+##        center = 
+##
+##        points = np.random.random((10,3))
+##        self.plot_points(points, size=0.2)
+##        
+##        self.finish_plotting()
+
+
+if __name__ == "__main__":
+##    win = nice_window("test")
+##    vp = Viewport3D(win, width=512, height=512)
+##    vp.get_widget().bind("t", vp.test, add="+")
+##    
+##    win.mainloop()
+    pass

py/_quick_visuals.py
@@ -3,12 +3,14 @@ import matplotlib.pyplot as plt
 
 from __points_and_masks import *
 
+from __system import function_timer
+
 def plotting_grid(width=1, height=1, figsize=(8,8)):
     fig,ax = plt.subplots(height, width, figsize=figsize)
     for i in range(width):
         for j in range(height):
             if width>1 and height>1: ax_target = ax[i][j]
-            elif width*height>1: ax_target = ax[i*width+j]
+            elif width>1 or height>1: ax_target = ax[i+j]
             else: ax_target = ax
             yield ax_target
 
@@ -28,6 +30,12 @@ class ImageDrawing:
     tl = None
     br = None
     
+    ndim = 0
+    
+    yaw = 0. # rotation around y axis
+    tilt = 0. # rotation around z axis
+    pitch = 0. # rotation around x axis
+    
     def __init__(self, *args, **kwargs):
         self.background = self._color_check(*args, **kwargs)
         self.objects = []
@@ -42,13 +50,18 @@ class ImageDrawing:
         self.colors.clear()
 
     def update_tl_br(self, tl, br):
+        ndim = tl.shape[0]
+        if ndim<self.ndim: # bounds were lower dimensional
+            tl = np.pad(tl, (0,self.ndim-ndim))
+            br = np.pad(br, (0,self.ndim-ndim))
+            tl[ndim:] = self.tl[ndim:]
+            br[ndim:] = self.br[ndim:]
+            
         if self.tl is None: self.tl = tl
         else: self.tl = np.min([tl,self.tl], axis=0)
         if self.br is None: self.br = br
         else: self.br = np.max([br,self.br], axis=0)
-
-    def get_extent(self):
-        return (self.tl[0], self.br[0], self.tl[1], self.br[1])
+        self.ndim = self.tl.shape[0]
         
     def points(self, points, *args, **kwargs):
         points = np.asarray(points, dtype=np.float64)
@@ -83,29 +96,10 @@ class ImageDrawing:
         self.colors.append(self._color_check(*args, **kwargs))
     
     def draw(self, resolution:int, topright_is_the_positive_corner=True):
-        pixels = []
-        for x in self.objects:
-            if isinstance(x, self.LinesObj):
-                s = pixelize_points(x.starts, self.tl, self.br, resolution)
-                e = pixelize_points(x.ends, self.tl, self.br, resolution)
-                pixels.append((0, s, e))
-                
-            elif isinstance(x, self.CirclesObj):
-                c = pixelize_points(x.points, self.tl, self.br, resolution)
-                radii = pixelize_distances([x.radius, x.inside], self.tl, self.br, resolution)
-                m = circle_mask(*radii)
-                pixels.append((1, c, m))
-                
-            elif isinstance(x, self.GridObj):
-                c = x.center
-                s = x.size
-                pixels.append((2, c, s))
-                
-            elif type(x)==np.ndarray:
-                pixels.append(pixelize_points(x, self.tl, self.br, resolution))
+        tl, br = self.get_rotated_bounds()
+        camera_pos = np.mean([tl,br], axis=0)
         
-        limits = pixelize_points(self.br, self.tl, self.br, resolution)
-        
+        limits = pixelize_points(br, tl, br, resolution)
         image = np.zeros((*image_shape(resolution, *limits), len(self.background)))
         image[:,:] = self.background
         
@@ -130,9 +124,9 @@ class ImageDrawing:
                 masks[key] = mask
             
             if mask is None: return False
-            tl = np.min([start,end], axis=0)
-            x_slice = slice(tl[0], tl[0]+mask.shape[0])
-            y_slice = slice(tl[1], tl[1]+mask.shape[1])
+            line_tl = np.min([start,end], axis=0)
+            x_slice = slice(line_tl[0], line_tl[0]+mask.shape[0])
+            y_slice = slice(line_tl[1], line_tl[1]+mask.shape[1])
             
             image[x_slice, y_slice][mask] = color_blend_on_array(image[x_slice, y_slice][mask], color)
             return True
@@ -143,30 +137,41 @@ class ImageDrawing:
             y_slice = slice(center[1]-r, center[1]+r+1)
             image[x_slice, y_slice][mask] = color_blend_on_array(image[x_slice, y_slice][mask], color)
         
-        for index,x in enumerate(pixels):
+        for index,x in enumerate(self.objects):
             color = self.colors[index]
-            if type(x)==tuple:
-                match x[0]:
-                    case 0: # lines
-                        for i,s in enumerate(x[1]):
-                            draw_line_on_image(s, x[2][i], color)
-                    case 1: # circles
-                        for c in x[1]:
-                            draw_circle_on_image(c, x[2], color)
-                    case 2: # grid
-                        grid_lines_n = int(np.divide(self.br-self.tl, x[2]).max())+2
-                        offset = np.divide(x[1]-self.tl, x[2]).astype(np.int32)
-                        for i in range(grid_lines_n):
-                            i -= offset
-                            target = x[2]*i
-                            target = pixelize_points(target, self.tl, self.br, resolution)
-                            
-                            if target[0]>=0 and target[0]<image.shape[0]:
-                                color_blend_on_array(image[target[0],:], color)
-                            if target[1]>=0 and target[1]<image.shape[1]:
-                                color_blend_on_array(image[:,target[1]], color)
-            else: # points
-                image[x[:,0],x[:,1]] = color_blend_on_array(image[x[:,0],x[:,1]], color)
+            if isinstance(x, self.LinesObj):
+                starts = self.rotate_points(x.starts, camera_pos)
+                ends = self.rotate_points(x.ends, camera_pos)
+                starts = pixelize_points(starts, tl, br, resolution)
+                ends = pixelize_points(ends, tl, br, resolution)
+                for i,s in enumerate(starts):
+                    draw_line_on_image(s, ends[i], color)
+                
+            elif isinstance(x, self.CirclesObj):
+                centers = self.rotate_points(x.points, camera_pos)
+                centers = pixelize_points(centers, tl, br, resolution)
+                radii = pixelize_distances([x.radius, x.inside], tl, br, resolution)
+                mask = circle_mask(*radii)
+                for c in centers:
+                    draw_circle_on_image(c, mask, color)
+                
+            elif isinstance(x, self.GridObj):
+                grid_lines_n = int(np.divide(br-tl, x.size).max())+2
+                offset = np.divide(x.center-tl, x.size).astype(np.int32)
+                for i in range(grid_lines_n):
+                    i -= offset
+                    target = x.size*i
+                    target = pixelize_points(target, tl, br, resolution)
+                    
+                    if target[0]>=0 and target[0]<image.shape[0]:
+                        color_blend_on_array(image[target[0],:], color)
+                    if target[1]>=0 and target[1]<image.shape[1]:
+                        color_blend_on_array(image[:,target[1]], color)    
+                
+            elif type(x)==np.ndarray:
+                x = self.rotate_points(x, camera_pos)
+                pixels = pixelize_points(x, tl, br, resolution)
+                image[pixels[:,0],pixels[:,1]] = color_blend_on_array(image[pixels[:,0],pixels[:,1]], color)
         
         if image.any():
             image /= image.max()
@@ -185,9 +190,54 @@ class ImageDrawing:
         self.draw_to_plot(plt, resolution)
         plt.show()
 
+    
+    def get_rotated_bounds(self):
+        bounds = np.array([self.tl, self.br])
+        corners = np.array(list(bounding_corners(bounds)))
+        rotated_corners = self.rotate_points(corners, center=np.mean(bounds, axis=0))
+        return bounding_box(rotated_corners)
+    
+    def rotate_points(self, points, center):
+        p_ndim = points.shape[1]
+        
+        if p_ndim<self.ndim:
+            new_points = np.pad(points, ((0,0),(0,self.ndim-p_ndim)))
+            new_points[:,p_ndim:] = center[p_ndim:] # fill with center values
+        else:
+            new_points = points.copy()
+        
+        new_points -= center
+        if self.ndim>2: # 3D
+            new_points = rotate_vectors_3d(new_points, self.pitch, self.yaw, self.tilt)
+        else:
+            new_points = rotate_vectors(new_points, self.tilt)
+        new_points += center
+        return new_points
+    
+    def get_unrotated_extent(self):
+        return (self.tl[0], self.br[0], self.tl[1], self.br[1])
+
+    def get_extent(self):
+        tl, br = self.get_rotated_bounds()
+        return (tl[0], br[0], tl[1], br[1])
+
+
 
 if __name__ == "__main__":
     drawing = ImageDrawing(r=1, g=1, b=1)
     drawing.points(np.random.random((100,3)))
-    drawing.test_draw(200)
+    drawing.lines(*point_lines(np.random.random((100,3))), b=1)
+    drawing.circles(np.random.random((100,2))+2, 0.05, r=1)
+    
+##    print((drawing.draw(512) == drawing.draw2(512)).all())
+##    drawing.tilt = np.pi/4
+##    drawing.test_draw(200)
+##    drawing.tilt = np.pi/4
+    for plot in plotting_grid(9, 1, (20,4)):
+        drawing.draw_to_plot(plot, 200)
+        drawing.yaw += np.pi/8
+        drawing.pitch += np.pi/8
+    plt.show()
+        
+        
     