update + graphical_interface viewport rotation controls + bezier curve functions + minor fixes

py/graphical_interface.py
@@ -23,6 +23,10 @@ class ModelInstance():
     step = None # -> IntegerField
     step_data_label = None # -> label
     auto_extend = None # -> BooleanSwitch
+
+    yaw = None # -> IntegerField
+    tilt = None # -> IntegerField
+    pitch = None # -> IntegerField
     
     def __init__(self, on_destroy, **kwargs):
         for k,v in kwargs.items():
@@ -175,6 +179,14 @@ class ModelInstance():
         #
 
         #
+        width = 4
+        ff = nice_titled_frame(f, "rotation", side=tk.TOP, anchor="nw", fill=tk.Y)
+        self.pitch = IntegerField(ff, low=0, mod=360, width=width, label_text="pitch", justify="center")
+        self.yaw = IntegerField(ff, low=0, mod=360, width=width, label_text="yaw", justify="center")
+        self.tilt = IntegerField(ff, low=0, mod=360, width=width, label_text="tilt", justify="center")
+        #
+
+        #
         ff = nice_titled_frame(f, "colors", side=tk.TOP)
         
         self.colors = {
@@ -196,9 +208,9 @@ class ModelInstance():
     def draw(self, resolution:int):
         
         drawing = ImageDrawing()
-##        drawing.yaw = 0
-##        drawing.tilt = 0
-##        drawing.pitch = 0
+        drawing.yaw = self.yaw.get()/180*np.pi
+        drawing.tilt = self.tilt.get()/180*np.pi
+        drawing.pitch = self.pitch.get()/180*np.pi
         drawing.set_color(*np.divide(self.colors["background"], 255))
         
         color = np.divide(self.colors["boundary"], 255)
@@ -253,12 +265,12 @@ class ModelInstance():
             obj.set_color(*color)
         
         image = drawing.draw(resolution)
-        return image, drawing.tl, drawing.br
+        return image, drawing.get_extent()#drawing.tl, drawing.br
         
     def save_png(self):
         path = os.path.join(SAVEDIR, "test.png")
         makedirs(path)
-        image, _, _ = self.draw(self.png_resolution.get())
+        image, _ = self.draw(self.png_resolution.get())
         PIL_image_from_array(image).save(path, optimize=True)
     
     def model_reset(self):
@@ -307,10 +319,10 @@ class ModelInstance():
         
     def refresh_viewport(self):
         if self.model.has_points():
-            image, tl, br = self.draw(self.fig_resolution.get())
+            image, extent = self.draw(self.fig_resolution.get())
             title = f"{len(self.model)} points"
             title += f"\nimage: {image.shape[:2]}"
-            self.viewport.update(image, tl, br, title=title)
+            self.viewport.update(image, extent, title=title)
         else:
             self.viewport.clear()
 

py/testing.py
@@ -76,20 +76,55 @@ def hausdorff_distance_testing():
         n <<= 1
 
 
-
-if __name__ == "__main__":
-##    hausdorff_distance_testing()
-    
+def image_drawing_3d_testing():
     dome = point_ball(12, 1)
     drawing = ImageDrawing()
     
-##    drawing.pitch = np.pi/3
-##    drawing.yaw = np.pi/16
-##    drawing.tilt = np.pi/16
+    drawing.pitch = np.pi/3
+    drawing.yaw = np.pi/16
+    drawing.tilt = np.pi/16
 
     for circle in dome:
         obj = drawing.lines(*point_lines(circle))
         obj.set_color(r=1)
         obj.set_color_bg(b=1, a=0.1)
         
-    drawing.test_draw(1000, camera_dist=0)  # 
+    drawing.test_draw(1000, camera_dist=2)  #
+
+
+def bezier_curve_testing():
+    # curve
+    point1 = (-1,1)
+    normal1 = (-1,1)
+    
+    point2 = (1,0)
+    normal2 = (3,1)
+    #
+
+    tangents = rotate_vectors(np.array((normal1, normal2)), np.pi/2)
+    curve_intsect = bezier_curve_intersection(point1, point2, *tangents)
+    curve = bezier_curve(point1, point2, curve_intsect, 50)
+
+    drawing = ImageDrawing()
+    obj = drawing.circles([curve_intsect], 0.03)
+    
+    obj = drawing.circles([point1, point2], 0.03)
+    obj.set_color(b=1)
+    
+    obj = drawing.lines([point1,point1,point2], [point2,curve_intsect,curve_intsect])
+    obj.set_color(b=1)
+    
+    obj = drawing.circles(curve, 0.02)
+    obj.set_color(r=1)
+    
+    drawing.test_draw(1000)
+
+
+
+
+
+if __name__ == "__main__":
+##    hausdorff_distance_testing()
+##    image_drawing_3d_testing()
+##    bezier_curve_testing()
+    pass